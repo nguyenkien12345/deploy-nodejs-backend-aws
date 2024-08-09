93f0aa8477b30f8a6b6cb53941f71ef6894035e7
Nguyễn Trung Kiên <kiennt@pivot.vn>
2024-08-09 13:24:11 +0700

    Update Api

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,16 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+router.get('/api/users', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'api ok',
+        data: [
+            { id: 1, name: 'Nguyen Trung Kien', age: 24, gender: true },
+            { id: 2, name: 'Nguyen Van A', age: 100, gender: true },
+            { id: 3, name: 'Nguyen Van B', age: 80, gender: false }
+        ]
+    })
+})
+
 module.exports = router;
